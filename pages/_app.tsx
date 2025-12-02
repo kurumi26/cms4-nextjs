5318e69074b49941267baeb9dc497f2327cac986
thugtech97 <juhde.1997@gmail.com>
2025-12-02 11:46:15 +0800

    hotfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-
+// pages/_app.tsx
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import type { AppProps } from 'next/app';
-import React, { useEffect } from 'react';
+import React from 'react';
+import Script from 'next/script';
 
 type AppPropsWithLayout = AppProps & {
   Component: AppProps['Component'] & {
@@ -11,14 +12,16 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-useEffect(() => {
-        import('bootstrap/dist/js/bootstrap.bundle.min.js');
-  }, []);
   const Layout = Component.Layout || React.Fragment;
 
   return (
     <Layout>
       <Component {...pageProps} />
+      
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        strategy="afterInteractive"
+      />
     </Layout>
   );
 }
